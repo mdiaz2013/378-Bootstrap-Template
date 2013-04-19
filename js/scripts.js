561fd63c3eafc32c940371af3cee2296851c3769
mayra diaz <may_day1215@hotmail.com>
2013-04-18 17:53:37 -0700

    4.18.13 Video Lecture
    added nav, links, thumbnails
    added code to html file & script file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-  
+ 
   // Good Stuff
+  var $lastLink=$('.nav-pills>li:last-child>a');
   
-});	
+  $lastLink.fadeOut('slow').fadeIn('fast');
+  
+  $lastLink.html('<i class="icon-star"></i>Last Link');
+  
+ 	$('h1').click(function() {
+  	$(this).hide();
+	});
+
+
+
+
+
+});
